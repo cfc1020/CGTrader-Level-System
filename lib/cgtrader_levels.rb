4cd96371587d1084d792307102b5e03b5ada0502
cfc1020 <cfc9209@gmail.com>
2015-12-22 17:47:11 +0300

    Added new fuatures

--- lib/cgtrader_levels.rb
+++ lib/cgtrader_levels.rb
@@ -3,4 +3,5 @@ require 'cgtrader_levels/version'
 module CgtraderLevels
   autoload :User, 'cgtrader_levels/models/user'
   autoload :Level, 'cgtrader_levels/models/level'
+  autoload :LevelChanger, 'cgtrader_levels/models/level_changer'
 end
